f8c4f12c798270fcca67d7ce708e20cda60338b8
Morampudi Surya Sai <suryasaiofficial@gmail.com>
2026-05-13 15:23:57 +0530

    Add files via upload

---
+++ Day-1/Variables.py
@@ -0,0 +1,119 @@
+Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+a=10
+print(a)
+10
+d=50
+print(D)
+Traceback (most recent call last):
+  File "<pyshell#3>", line 1, in <module>
+    print(D)
+NameError: name 'D' is not defined. Did you mean: 'd'?
+print(d)
+50
+z=60
+5=10
+SyntaxError: cannot assign to literal here. Maybe you meant '==' instead of '='?
+a5=20
+print(a5)
+20
+10a=20
+SyntaxError: invalid decimal literal
+a0123456789=100
+print(a0123456789)
+100
+if=70
+SyntaxError: invalid syntax
+for=50
+SyntaxError: invalid syntax
+while=30
+SyntaxError: invalid syntax
+city="vij"
+print(city)
+vij
+print("city")
+city
+country="india
+SyntaxError: unterminated string literal (detected at line 1)
+country="india"
+print(country)
+india
+mobileno=9030288756
+print(mobileno)
+9030288756
+fname="surya"
+lname="sai"
+print(fname+" "+lname)
+surya sai
+print(fname,lname)
+surya sai
+a=10,b=20
+SyntaxError: invalid syntax. Maybe you meant '==' or ':=' instead of '='?
+a=10;b=20
+print(a,b)
+10 20
+a,b=10,20
+print(a,b)
+10 20
+print(a+b)
+30
+print(a-b)
+-10
+a=2,3,4,5,6
+print(a)
+(2, 3, 4, 5, 6)
+a,b,c=(6,7,8)
+print(a,b,c)
+6 7 8
+z=10
+print(z)
+10
+del(z)
+print(z)
+Traceback (most recent call last):
+  File "<pyshell#41>", line 1, in <module>
+    print(z)
+NameError: name 'z' is not defined
+print(a)
+6
+@=100
+SyntaxError: invalid syntax
+$=1000
+SyntaxError: invalid syntax
+_=40
+print(_)
+40
+ =50
+ 
+SyntaxError: unexpected indent
+first name="surya"
+SyntaxError: invalid syntax
+first_name="surya"
+firstname="surya"
+print(first_name)
+surya
+print(firstname)
+surya
+first name="surya
+SyntaxError: unterminated string literal (detected at line 1)
+age=10
+print(age)
+10
+AGE=10
+>>> print(AGE)
+10
+>>> Age=20
+>>> primt(Age)
+Traceback (most recent call last):
+  File "<pyshell#59>", line 1, in <module>
+    primt(Age)
+NameError: name 'primt' is not defined. Did you mean: 'print'?
+>>> print(Age)
+20
+>>> first@name="surya"
+SyntaxError: cannot assign to expression here. Maybe you meant '==' instead of '='?
+>>> mail="suryasaiofficial@gmail.com
+SyntaxError: unterminated string literal (detected at line 1)
+>>> mail="suryasaiofficial@gmail.com"
+>>> print(mail)
+suryasaiofficial@gmail.com
