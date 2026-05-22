0f31da9cee0e29d2b522c4bebe28194ca53d2e0d
Morampudi Surya Sai <suryasaiofficial@gmail.com>
2026-05-22 21:18:25 +0530

    Add files via upload

---
+++ Day10/conditional statements.py
@@ -0,0 +1,99 @@
+#if-elif-else conditions by using comparision operator
+'''a=10
+b=15
+if a<b:
+    print("less")
+elif b>a:
+    print("greater")'''
+
+'''a=10
+b=20
+if a>b:
+    print("true")'''
+
+'''a=5
+b=10
+if a>b:
+    print("less")
+elif b>a:
+    print("greater")
+else:
+    print("true")'''
+
+'''a=4
+b=8
+if a==b:
+    print("less")
+elif b>a:
+    print("greater")
+elif a!=b:
+    print("not equal to")
+else:
+    print("true")'''
+
+
+
+
+'''
+
+a=int(input())
+b=int(input())
+c=[10,20,30]
+if a>b:
+    print("less")
+elif b>a and b!=a:
+    print("greater")
+elif a is b:
+    print("not equal to")
+elif a in b:
+    print("equal")
+else:
+    print("error")'''
+
+
+#____________________________________________________________________________________________________________________________
+
+#multiple-if
+'''a=4
+b=9
+if a<b:
+    print("less")
+if b>a:
+    print("greater")
+if a==b:
+    print("error")'''
+
+a=int(input())
+b=int(input())
+c=[1,2,3,4,5,6,7,8,9,10]
+if a>b:
+    print(
+      
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
